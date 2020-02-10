41a322c3631fceb6319e26a2e8212a9462149b4e
amberstyers <amberstyers@gmail.com>
2020-02-10 17:12:53 -0600

    almost done

--- src/Router.js
+++ src/Router.js
@@ -12,6 +12,7 @@ import React from 'react'
 import { Switch, Route } from 'react-router'
 import Home from './components/Home'
 import About from './components/About'
+import Car from './components/Car'
 // Write component imports here //
 
 
@@ -23,6 +24,7 @@ const Router = () => {
         <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
+            <Route path="/car/:id" component={Car} />
         </Switch>
     )
 }
